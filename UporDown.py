0d73b34780d126fa07b712a0641b96cc381749bc
Programlog <varunk3249@gmail.com>
2024-04-20 23:06:25 -0400

    added sentiment functions right into script

--- UporDown.py
+++ UporDown.py
@@ -3,7 +3,40 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import sys
-from sentiment import get_sentiment, get_info
+from textblob import TextBlob
+
+
+def get_sentiment(name):
+    name = name.lower()
+    ticker = yf.Ticker(name)
+
+    long_name = ticker.info.get('longName').lower().split()[0]
+
+    news = ticker.news
+
+    sum = 0
+    num_news = 1
+    for article in news:
+        title = article.get("title").lower()
+        if (name in title) or (long_name in title):
+            sum += TextBlob(title).sentiment.polarity
+            num_news += 1
+
+    return sum/num_news
+
+
+def get_info(name):
+    ticker = yf.Ticker(name)
+    info = ticker.info
+
+    data = {"forwardEps": info.get('forwardEps'), 
+            "marketCap": info.get('marketCap'), 
+            "trailingPE": info.get('trailingPE'), 
+            "recommendationKey": info.get('recommendationKey')}
+    return data
+
+
+
 
 arguements = sys.argv
 name = arguements[1]
